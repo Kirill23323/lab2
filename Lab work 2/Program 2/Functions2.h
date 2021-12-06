afac3704b9a7941172cfdb8f110ab5d5fbdd4586
Kirill23323 <90023656+Kirill23323@users.noreply.github.com>
2021-12-06 23:46:27 +0300

    Add files via upload

---
+++ Lab work 2/Program 2/Functions2.h
@@ -0,0 +1,18 @@
+#ifndef __FUNCTIONS_H__
+#define __FUNCTIONS_H__
+
+
+// Program 2
+
+void ZeroingAnArray(int n, float** mas1);
+void BubbleSort(int N, float* mas1);
+void InsertSort(int N, float* mas1);
+void QuickSort(float* mas1, int first, int last);
+void PrintArray(int n, float* mas1);
+void FirstNorm(int n, float* mas);
+void SecondNorm(int n, float* mas);
+int HelderNorm(int n, float* mas,int p);
+void InfiniteNorm(int n, float* mas);
+void Rationing(int n, float* mas);
+
+#endif
